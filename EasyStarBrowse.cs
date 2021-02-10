3d15e33be09c1c0cf75c2216cdad2c5f08ffb533
jinx <49783661+jinxOAO@users.noreply.github.com>
2021-02-10 14:39:05 +0800

    Add files via upload

---
+++ EasyStarBrowse.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+using BepInEx.Configuration;
+
+namespace EasyStarBrowse
+{
+    [BepInPlugin("GniMaerd.DSP.plugin.EasyStarBrowse", "EasyStarBrowse", "1.0")]
+    public class EasyStarBrowse:BaseUnityPlugin
+    {
+
+        public static ConfigEntry<KeyCode> KeyConfig;
+        public static bool PatchDist = true;
+        void Start()
+        {
+            KeyConfig = Config.Bind("config", "HotKey", KeyCode.O, "热键，在游戏中切换是否启用此mod");
+            Harmony.CreateAndPatchAll(typeof(EasyStarBrowse));
+        }
+        void Update()
+        {
+            if(Input.GetKeyDown(KeyConfig.Value))
+            {
+                PatchDist = !PatchDist;
+            }
+        }
+        
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(StarmapCamera), "SetViewTarget", new Type[] { typeof(PlanetData), typeof(StarData), typeof(Player), typeof(VectorLF3), typeof(Quaternion), typeof(double), typeof(double), typeof(bool), typeof(bool) } )]
+        public static bool ViewPatch(ref double maxDist)
+        {
+            if(!PatchDist)
+            {
+                return true;
+            }
+            //var _this = __instance;
+            //_this.dist = 99;
+            maxDist = 35000f;
+            
+            return true;
+        }
+        
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(StarmapCamera), "SetViewTarget", new Type[] { typeof(PlanetData), typeof(StarData), typeof(Player), typeof(VectorLF3), typeof(double), typeof(double), typeof(bool), typeof(bool) })]
+        public static bool ViewPatch2(ref double maxDist)
+        {
+            if(!PatchDist)
+            {
+                return true;
+            }
+            //var _this = __instance;
+            //_this.dist = 99;
+            maxDist = 35000f;
+            
+            return true;
+        }
+
+    }
+}
