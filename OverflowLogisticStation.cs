9afd95ce13db13e5048392ddd9b45fc55069a460
jinx <49783661+jinxOAO@users.noreply.github.com>
2022-02-04 07:08:09 +0800

    Add files via upload

---
+++ OverflowLogisticStation.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using System.IO;
+
+namespace OverflowLogisticStation
+
+{
+    [BepInPlugin("GniMaerd.DSP.plugin.OverflowLogisticStation", "OverflowLogisticStation", "1.0")]
+    public class OverflowLogisticStation : BaseUnityPlugin
+    {
+        private static ConfigEntry<bool> VeinCollectorOverflow;
+        void Start()
+        {
+            VeinCollectorOverflow = Config.Bind<bool>("config", "AdvancedMiningMachineOverflow", false, "If set to true, the advanced mining machine can overflow when set to local supply mode. 如果设置成true，大型采矿机将在设置为本地供给允许溢出。");
+            Harmony.CreateAndPatchAll(typeof(OverflowLogisticStation));
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(StationComponent), "UpdateNeeds")]
+        public static void UpdateNeedsPatch(ref StationComponent __instance)
+        {
+            
+            var _this = __instance;
+            int overflowAmount = 1;//每帧如果满了。当前仓储设置为 max - overflowAmout，默认为max-1，但对于大矿机比较特殊
+            if(_this.isVeinCollector)//如果是大矿机
+            {
+                if (VeinCollectorOverflow.Value)//如果允许溢出，大矿机的采集速度可能超过每帧1个，这样还是有可能停工，除非每帧修改成max-10这样减的更多一些
+                {
+                    overflowAmount = 10;
+                }
+                else//否则返回
+                {
+                    return;
+                }
+            }
+            StationStore[] obj = _this.storage;
+            lock (obj)
+            {
+                int num = _this.storage.Length;
+                for (int i = 0; i < num && i < 5; i++)
+                {
+                    if(_this.storage[i].localLogic == ELogisticStorage.Supply && ( !_this.isStellar || _this.storage[i].remoteLogic == ELogisticStorage.Supply) )//对于星际，必须都是供给才允许溢出，对于行星内物流站，则只需要本地供给就允许溢出
+                    {
+                        _this.storage[i].count = _this.storage[i].count >= _this.storage[i].max ? _this.storage[i].max - overflowAmount : _this.storage[i].count;
+                    }
+                }
+            }
+        }
+    }
+}
